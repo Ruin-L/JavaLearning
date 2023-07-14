b03e4ea388964a05876c2ee097df5f691313bd16
刘引 <liu.yin.work@foxmail.com>
2023-07-14 17:52:04 +0800

    学习文档提交

---
+++ Hello/src/src/Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("主方法");
+        test("传值");
+
+    }
+    public static void test(String S) {
+        System.out.println(S);
+    }
+}
